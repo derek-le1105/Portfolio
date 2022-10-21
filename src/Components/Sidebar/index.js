857764157d069e7e53a1d1194449e4c11721cda8
derek-le1105 <derekle1105@gmail.com>
2022-10-20 23:26:07 -0700

    test3

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -13,14 +13,14 @@ const Sidebar = () => (
             <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" to="/Portfolio">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
             <NavLink
                 exact="true"
                 activeclassname="active"
                 className="about-link"
-                to="/about"
+                to="/Portfolio/about"
             >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
@@ -28,7 +28,7 @@ const Sidebar = () => (
                 exact="true"
                 activeclassname="active"
                 className="contact-link"
-                to="/contact"
+                to="/Portfolio/contact"
             >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
