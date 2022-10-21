bafd29dde0db022d156909b5729295b41b8c4139
derek-le1105 <derekle1105@gmail.com>
2022-10-21 14:35:16 -0700

    test

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import './index.scss'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
